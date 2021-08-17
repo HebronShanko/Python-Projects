d359cc469ed837824c1f9920bd53266b8194f8da
HebronShanko <hebronshanko@gmail.com>
2021-08-17 12:13:02 -0700

    Create classes.py

---
+++ Python/classes.py
@@ -0,0 +1,20 @@
+class Robot:
+    name = 'Mac'
+    color = 'Red'
+    weight = 40
+
+# created class, with three attributes
+
+class read(Robot):
+    name = 'readBot'
+    color = 'blue'
+    weight = 50
+
+class write(Robot):
+    name = 'writeBot'
+    color = 'black'
+    weight = 55
+
+# child of class with own attributes
+
+    
